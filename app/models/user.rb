e79cc590c24c63e1644dd08e7ca33eeb47970d6b
Howard Yeh <hayeah@gmail.com>
2010-05-18 23:08:10 -0700

    can't get email to work on heroku, so disable confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :lockable and :timeoutable
-  devise :database_authenticatable, :registerable, :confirmable,
+  devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable
 
   validates_presence_of :name
