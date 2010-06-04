eba5f35b1bbbff6e0c8efcaa33d311d013fa26e4
Howard Yeh <hayeah@gmail.com>
2010-06-04 00:02:30 -0700

    some more tweaks for password input

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,12 @@
 $(window).load(function () {
+    var password_field_hint = "<input id='user_password' name='user[password]' size='30' type='text' value='password'/>";
+    var password_field = "<input id='user_password' name='user[password]' size='30' type='password' value=''/>";
+    $("#user_password").replaceWith(password_field_hint);
+    $("#user_password").bind({
+        focusin: function() {
+            $(this).replaceWith(password_field);
+        }
+    });
     $("#login input").bind({
         focusin: function() {
             $(this).val("");
