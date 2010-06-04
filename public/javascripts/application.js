13b263ca9c361a85332652cedf783db832cd23fc
Howard Yeh <hayeah@gmail.com>
2010-06-04 16:24:06 -0700

    login tabindex

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,13 @@
 $(window).load(function () {
-    var password_field_hint = "<input id='user_password' name='user[password]' size='30' type='text' value='password'/>";
-    var password_field = "<input id='user_password' name='user[password]' size='30' type='password' value=''/>";
+    var password_field_hint = "<input id='user_password' name='user[password]' size='30' type='text' value='password' tabindex='2'/>";
+    var password_field = "<input id='user_password' name='user[password]' size='30' type='password' value='' tabindex='2'/>";
     $("#user_password").replaceWith(password_field_hint);
     $("#user_password").bind({
         focusin: function() {
             var replacement = $(password_field);
             replacement.css("color","#000");
             $(this).replaceWith(replacement);
+            replacement.focus();
             $(this).unbind("focusin");
         }
     });
