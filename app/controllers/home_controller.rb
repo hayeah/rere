fdf87b082bbce66d2609312c1b1aac0d1c4e13f8
Vincent Chu <vinceh@Vincent-Chus-MacBook-Pro.local>
2010-06-05 00:20:27 -0700

    new branch

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   # tweet stream
   def index
     if user_signed_in?
-      redirect_to stream_path(:username => current_user.username)
+      render "splash"
     else
       @user = User.new
       render "splash"
