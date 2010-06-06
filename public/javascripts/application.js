47b331f9a30a33b6266685e42f51eb8e5159c627
Howard Yeh <hayeah@gmail.com>
2010-06-05 21:57:45 -0700

    more specific jquery selector for quick login to avoid clobbering other pages

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,8 +2,8 @@
 $(window).load(function () {
     var password_field_hint = "<input id='user_password' name='user[password]' size='30' type='text' value='password' tabindex='2'/>";
     var password_field = "<input id='user_password' name='user[password]' size='30' type='password' value='' tabindex='2'/>";
-    $("#user_password").replaceWith(password_field_hint);
-    $("#user_password").bind({
+    $("#header #user_password").replaceWith(password_field_hint);
+    $("#header #user_password").bind({
         focusin: function() {
             var replacement = $(password_field);
             replacement.css("color","#000");
@@ -12,7 +12,7 @@ $(window).load(function () {
             $(this).unbind("focusin");
         }
     });
-    $("#user_username").bind({
+    $("#header #user_username").bind({
         focusin: function() {
             $(this).css("color","black");
             $(this).val("");
