3327621d2609e8c8a700674c9327fbc4ba6caaaa
Howard Yeh <hayeah@gmail.com>
2010-06-06 17:25:05 -0700

    set profile pic in user settings

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,13 @@
 class RegistrationsController < Devise::RegistrationsController
   prepend_view_path "app/views/devise"
   layout "box"
+
+  def update
+    if resource.update_with_password(params[resource_name])
+      set_flash_message :notice, :updated
+    else
+      clean_up_passwords(resource)
+    end
+    render :edit
+  end
 end
