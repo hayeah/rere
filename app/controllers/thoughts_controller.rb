4db52f2c5a18b9bbd94ba3a5cec3639f17037c0c
Howard Yeh <hayeah@gmail.com>
2010-06-15 22:58:58 -0700

    thoughts#show

--- app/controllers/thoughts_controller.rb
+++ app/controllers/thoughts_controller.rb
@@ -1,3 +1,7 @@
 class ThoughtsController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :except => :show
+  def show
+    @thought = Thought.find(params[:id],:include => [{:comments => :user},:user])
+    render :layout => "box"
+  end
 end
